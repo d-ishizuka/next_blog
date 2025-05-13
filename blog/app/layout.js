078bd37b9101cdb72453aa539f1eb6372975cff8
d-ishizuka <vbstkaksa.0716@gmail.com>
2025-05-13 09:18:07 +0900

    common layout

--- blog/app/layout.js
+++ blog/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Layout from "@/components/layout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,9 +19,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+    <html lang="ja" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
+        <Layout>
+          <main>{children}</main>
+        </Layout>
       </body>
     </html>
   );
--- blog/app/page.js
+++ blog/app/page.js
@@ -1,14 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "@/components/hero";
-import Layout from "@/components/layout";
 
 export default function Home() {
   return (
     <>
-      <Layout>
-        <Hero />
-      </Layout>
+      <Hero />
     </>
   )
 }
