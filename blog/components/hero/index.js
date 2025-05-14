8d489d9c5179e87df412e7c47f8d97d0db4327f9
d-ishizuka <vbstkaksa.0716@gmail.com>
2025-05-14 22:13:59 +0900

    add hero css

---
+++ blog/components/hero/index.js
@@ -0,0 +1,13 @@
+import styles from "@/components/hero/styles.module.css";
+
+export default function Hero({ title, subtitle, imageOn = false }) {
+  return (
+    <div>
+      <div className={styles.text}>
+        <h1 className={styles.title}>{ title }</h1>
+        <p className={styles.subtitle}>{ subtitle }</p>
+        {imageOn && <figure> [画像] </figure>}
+      </div>
+    </div>
+  );
+}
