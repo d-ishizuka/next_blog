b051bbc0b2a28bdae7a03a7a74b9f33c94d45d3d
d-ishizuka <vbstkaksa.0716@gmail.com>
2025-06-15 12:39:27 +0900

    add fontawesome setting

--- blog/app/layout.js
+++ blog/app/layout.js
@@ -2,6 +2,18 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Layout from "@/components/layout";
 
+// フォントの設定
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHouseChimney } from '@fortawesome/free-solid-svg-icons';
+import { faSun } from '@fortawesome/free-regular-svg-icons';
+import { faTwitter, faFacebookF} from '@fortawesome/free-brands-svg-icons';
+
+library.add(faHouseChimney, faSun, faTwitter, faFacebookF);
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
